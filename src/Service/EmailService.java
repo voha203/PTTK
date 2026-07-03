8ca20d13d4866ad2931a5e6ffe3ae048b878bc53
23130288 <23130288@st.hcmuaf.edu.vn>
2026-07-03 15:04:34 +0700

    forgot this actually, but it's done

--- src/Service/EmailService.java
+++ src/Service/EmailService.java
@@ -1,10 +1,42 @@
 package Service;
 
+import java.util.Properties;
+
 import Model.User;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 public class EmailService {
-	
+
 	public void notifyPasswordChanged(User user) {
-		// TODO
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("23130288@st.hcmuaf.edu.vn", "wacswtfmfirfnzxj");
+			}
+		});
+
+		Message message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress("23130288@st.hcmuaf.edu.vn"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail()));
+			message.setSubject("OTP Verification");
+			message.setText("Your password has changed");
+			Transport.send(message);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 	}
 }
