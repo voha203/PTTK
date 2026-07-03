a70f9111c98e3ec380667d4d2eec4ccefb24a3ed
Nguyễn Thanh Hải <23130086@st.hcmuaf.edu.vn>
2026-07-03 22:28:06 +0700

    xóa đăng nhập bằng fb

--- src/zRun/Main.java
+++ src/zRun/Main.java
@@ -4,18 +4,18 @@ import Controller.LoginController;
 import View.LoginPage;
 import View.RatingView;
 
-//public class Main {
-//
-//	public static void main(String[] args) {
-//		LoginPage loginPage = new LoginPage();
-//		LoginController controller = new LoginController(loginPage);
-//		loginPage.setController(controller);
-//		loginPage.setVisible(true);
-//	}
-//}
-
 public class Main {
+
 	public static void main(String[] args) {
-		new RatingView();
+		LoginPage loginPage = new LoginPage();
+		LoginController controller = new LoginController(loginPage);
+		loginPage.setController(controller);
+		loginPage.setVisible(true);
 	}
 }
+
+//public class Main {
+//	public static void main(String[] args) {
+//		new RatingView();
+//	}
+//}
